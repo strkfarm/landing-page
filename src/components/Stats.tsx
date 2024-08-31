08ab70ffd535abc533fc9eae86a331df6882c180
Hemant <hrsmart2150@gmail.com>
2024-08-31 17:03:16 +0530

    fix: tvl api

--- src/components/Stats.tsx
+++ src/components/Stats.tsx
@@ -1,4 +1,5 @@
 import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react';
 
 const endpoint = "https://indexer-graphql-api.onrender.com"
@@ -15,49 +16,40 @@ const REWARDS_QUERY = `
 `;
 
 const Stats = () => {
-    const { data, isLoading, error } = useQuery({
+    const { data: generatedRewardsData, isLoading, error } = useQuery({
         queryKey: ["total-rewards-generated"],
         queryFn: async () => {
-            return fetch(endpoint, {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({ query: REWARDS_QUERY })
+            const { data } = await axios.post(endpoint, {
+                query: REWARDS_QUERY
             })
-                .then((response) => {
-                    if (response.status >= 400) {
-                        console.error("Error fetching generated rewards");
-                    } else {
-                        return response.json();
-                    }
-                })
-                .then((data) => data.data);
+            return data?.data;
         }
     });
 
-    const { data: tvlData } = useQuery({
+    const { data: tvlData, isLoading: tvlLoading } = useQuery({
         queryKey: ["tvl"],
         queryFn: async () => {
-            return fetch('https://strkfarm.xyz/api/stats')
-                .then((response) => {
-                    if (response.status >= 400) {
-                        console.error("Error fetching tvl");
-                    } else {
-                        return response.json();
-                    }
-                })
-                .then((data) => data.data);
+            const { data } = await axios.get('https://starkfarm-client.vercel.app/api/stats')
+            return data?.tvl
         }
     });
 
-    console.log(tvlData)
+    if (error) {
+        console.error(error);
+    }
 
     return (
         <div className="mt-32 flex flex-col gap-10 lg:flex-row justify-around rounded-2xl bg-opacity-80 bg-gradient-to-r from-[#2E2C5C] to-[#295446] py-8">
             <div className="flex flex-col items-center justify-center gap-3">
                 <h4 className="text-white/90 text-lg">TVL</h4>
-                <span className="font-bold text-[#61EDAA] text-5xl">
-                    $419
-                </span>
+
+                {tvlLoading ? (
+                    <div className='h-10 w-1/2 animate-pulse bg-gradient-to-r from-[#36735e] to-[#295446] rounded-lg' />
+                ) : (
+                    <span className="font-bold text-[#61EDAA] text-5xl">
+                        ${(tvlData / 1000).toFixed(0)}k
+                    </span>
+                )}
             </div>
 
             <div className="flex flex-col items-center justify-center gap-3">
@@ -66,10 +58,10 @@ const Stats = () => {
                 </h4>
 
                 {isLoading ? (
-                    <div className='h-10 w-full animate-pulse bg-gradient-to-r from-[#36735e] to-[#295446] rounded-lg' />
+                    <div className='h-10 w-1/2 animate-pulse bg-gradient-to-r from-[#36735e] to-[#295446] rounded-lg' />
                 ) : (
                     <span className="font-bold text-[#61EDAA] text-5xl">
-                        ${(data?.totalStrkHarvested?.USDValue / 1000).toFixed(1)}k
+                        ${(generatedRewardsData?.totalStrkHarvested?.USDValue / 1000).toFixed(1)}k
                     </span>
                 )}
 
