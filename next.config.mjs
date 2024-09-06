14563630df9af18c31940ad835d3944ed472f44c
Hemant <hrsmart2150@gmail.com>
2024-09-06 22:14:18 +0530

    added redirections in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,39 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/r/:path*',
+            destination: 'https://app.strkfarm.xyz/r/:path*',
+            permanent: true,
+          },
+          {
+            source: '/slinks',
+            destination: 'https://app.strkfarm.xyz/slinks',
+            permanent: true,
+          },
+          {
+            source: '/strategy/:path*',
+            destination: 'https://app.strkfarm.xyz/strategy/:path*',
+            permanent: true,
+          },
+          {
+            source: '/eth',
+            destination: 'https://app.strkfarm.xyz/eth',
+            permanent: true,
+          },
+          {
+            source: '/strk',
+            destination: 'https://app.strkfarm.xyz/strk',
+            permanent: true,
+          },
+          {
+            source: '/usdc',
+            destination: 'https://app.strkfarm.xyz/usdc',
+            permanent: true,
+          },
+        ];
+      },
+};
 
 export default nextConfig;
